8e24a2e34f3b332cf365553aeba286602ae6ebfa
root <root@vultr.guest>
2021-01-13 05:17:00 +0000

    change

--- dist/admin/secure/js/14.85e3fadd1856f10be5b3.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{146:function(e,n,a){"use strict";a.r(n);var r=a(39),t=a(0),l=a.n(t);n.default=({onChange:e,filter:n,field:a,innerRef:t,value:u})=>{if(!n)return null;const i=a.getFilterLabel(n);return l.a.createElement(r.c,{onChange:({target:{value:n}})=>{e(n)},ref:t,placeholder:i,value:u})}}}]);
